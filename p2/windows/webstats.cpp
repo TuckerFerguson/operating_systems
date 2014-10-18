cb36d27d071f95571f368510ddd97e8eb146ebbb
Reuben Tanner <reubentanner@mobilebeast.local>
2014-10-18 11:05:14 -0600

    committing actual windows cpp file

--- p2/windows/webstats.cpp
+++ p2/windows/webstats.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pthread.h>
+#include <iostream>
 
 /*
 	Generate some web statistics  based on access logs from the Apache web server. 
