c487cdd755b463968258050be7423d03417f6227
Reuben Tanner <reubentanner@mobilebeast.local>
2014-10-27 19:37:54 -0600

    adding producers and consumers example

---
+++ p3/p3/ProducersAndConsumers.c
@@ -0,0 +1,32 @@
+//
+//  ProducersAndConsumers.c
+//  p3
+//
+//  Created by Reuben Tanner on 10/27/14.
+//  Copyright (c) 2014 Reuben Tanner. All rights reserved.
+//
+
+#include "ProducersAndConsumers.h"
+
+int main(int argc, char** argv)
+{
+    //argn = producers
+    //argn+1 = consumers
+    //argn+2 = amount to produce
+
+    //create global queue (linked list)
+    
+    //create m producers
+    //create n consumers
+    //consumer tracks its consumption
+    
+    //item produced/consumed
+    
+    
+    
+}
+
+struct item {
+    int id;
+    int created_by;
+};
